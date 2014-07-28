bf12eb312b88f2edc65311d98f51bcd80348083f
Christian Kakesa <christian.kakesa@gmail.com>
2014-07-29 01:44:56 +0200

    Add connection timeout for Redic (redis) instance.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -16,16 +16,14 @@ log_file = File.expand_path(File.join(log_dir, "joke_server_#{sinatra_env}.log")
 stderr_path log_file
 stdout_path log_file
 
-# before_fork do |server, worker|
+#before_fork do |server, worker|
 #
-# end
+#end
 
 after_fork do |server, worker|
   if defined?(Ohm)
     # default 'redis://localhost:6379/0'
-    if defined?(Ohm::Model)
-      Ohm.redis = Redic.new('redis://localhost:6379/0')
-    end
+    Ohm.redis = Redic.new('redis://localhost:6379/0', 30)
   end
 end
 
